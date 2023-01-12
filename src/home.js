ef21d08d3d198e2d8dd943b8e4817ab3aa2b4ec1
madaooftheblues <azlanrasool@gmail.com>
2023-01-13 03:46:37 +0500

    Implement tab switching logic

---
+++ src/home.js
@@ -0,0 +1,17 @@
+const home = (element) => {
+  const welcome = document.createElement("h2");
+  welcome.innerText = "Welcome to The Secret Garden Bistro";
+  welcome.classList.add("welcome");
+
+  const description = document.createElement("p");
+  description.innerText =
+    "The Secret Garden Bistro is a wonderful restaurant that offers a unique and delightful dining experience. Nestled among lush greenery and surrounded by a serene garden, the bistro offers a peaceful and tranquil atmosphere. The restaurant's menu is a delicious fusion of cuisines from around the world, featuring fresh, locally-sourced ingredients and an extensive selection of wines and cocktails. The chefs at The Secret Garden Bistro are experts in their craft, and are dedicated to creating flavorful and visually stunning dishes that will delight your senses.";
+  description.classList.add("description");
+
+  const home = document.createElement("div");
+  home.classList.add("home");
+  home.append(welcome, description);
+  element.append(home);
+};
+
+export default home;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Background from "./secret_garden_bistro.jpg";
+import home from "./home";
 import "./styles.css";
 
 const content = document.getElementById("content");
@@ -11,10 +12,51 @@ heading.innerText =
   "Discover the Hidden Gem: The Secret Garden Bistro - A Delicious Fusion of Cuisine and Nature";
 heading.classList.add("heading");
 
+const navbar = document.createElement("div");
+navbar.classList.add("navbar");
+
+const tabs = document.createElement("ul");
+tabs.classList.add("tabs");
+const homeTab = document.createElement("li");
+homeTab.innerText = "Home";
+const menuTab = document.createElement("li");
+menuTab.innerText = "Menu";
+const contactTab = document.createElement("li");
+contactTab.innerText = "Contact";
+
 const image = new Image();
 image.src = Background;
 image.classList.add("image");
 document.body.style.backgroundImage = `url(${image.src})`;
 document.body.style.backgroundRepeat = "no-repeat";
 document.body.style.backgroundSize = "cover";
-content.append(name, heading);
+
+const view = document.createElement("div");
+view.id = "view";
+
+tabs.append(homeTab, menuTab, contactTab);
+navbar.append(tabs);
+
+content.append(name, heading, navbar, view);
+
+let current = homeTab;
+
+function changeTab(e) {
+  current.classList.remove("current");
+  current = e.target;
+  current.classList.add("current");
+  view.innerText = "";
+}
+
+homeTab.addEventListener("click", (e) => {
+  changeTab(e);
+  home(view);
+});
+
+contactTab.addEventListener("click", (e) => {
+  changeTab(e);
+});
+
+menuTab.addEventListener("click", (e) => {
+  changeTab(e);
+});
